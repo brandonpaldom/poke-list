e81f2e5fdcf86162f4e2e0239aa0d490a7359dec
Brandon Palmeros <brandonpaldom@gmail.com>
2023-02-02 21:07:55 -0600

    New screen added

--- src/components/favorite-grid-container.js
+++ src/components/favorite-grid-container.js
@@ -2,7 +2,7 @@ import FavoriteGridItem from './favorite-grid-item'
 
 export default function FavoriteGridContainer({ pokemons }) {
   return (
-    <div className="grid min-h-screen grid-cols-2 content-start gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+    <div className="grid min-h-screen grid-cols-2 content-start gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
       {pokemons.map((pokemon) => (
         <FavoriteGridItem key={pokemon} pokemon={pokemon} />
       ))}
--- src/components/grid-container.js
+++ src/components/grid-container.js
@@ -2,7 +2,7 @@ import GridItem from './grid-item'
 
 export default function GridContainer({ pokemons }) {
   return (
-    <div className="grid min-h-screen grid-cols-2 content-start gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+    <div className="grid min-h-screen grid-cols-2 content-start gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
       {pokemons.map((pokemon) => (
         <GridItem key={pokemon.name} pokemon={pokemon} />
       ))}
